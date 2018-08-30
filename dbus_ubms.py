6ecd3dc4a4589120102f13e7ce10ce3035a1b7ee
gimx <einkauf.cn@gmail.com>
2018-08-30 21:29:32 +0000

    added basic decoding of UBMS messages

--- dbus_ubms.py
+++ dbus_ubms.py
@@ -15,20 +15,34 @@ import sys
 import os
 
 import can
+import struct
 
 # our own packages
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), 'ext/velib_python'))
 from vedbus import VeDbusService
 
-class CanbusDataAcquisition:
-    def __init__(self,interface):
-        self._bus = can.interface.Bus(channel=interface, bustype='socketcan')
+
+class UbmsBattery(can.Listener):
+    def __init__(self):
         self.soc = 0;
-        self.packvolatage = 0;
-        self.dischargecurrent = 0;
-        self.disschargecurrent = 0;
+        self.voltage = 0;
+	self.current = 0
         self.temperature = 0;
 
+    def on_message_received(self, msg):
+	if msg.arbitration_id == 0xc0:
+		self.soc = msg.data[0]
+		print("SOC: ",self.soc)
+
+	if msg.arbitration_id == 0xc1:
+                self.voltage = msg.data[0]
+                self.current = struct.unpack('b',chr(msg.data[1]))[0]
+		print("I: ",self.current, "U: ", self.voltage)
+
+	if msg.arbitration_id == 0xc4:
+		self.umax =  struct.unpack('<h', chr(msg.data[4])+chr(msg.data[5]))[0]*0.001
+		self.umin =  struct.unpack('<h', chr(msg.data[6])+chr(msg.data[7]))[0]*0.001
+		print("Umin ", self.umin, "Umax ", self.umax)
 
 
 class DbusBatteryService:
@@ -50,30 +64,26 @@ class DbusBatteryService:
         self._dbusservice.add_path('/FirmwareVersion', 0)
         self._dbusservice.add_path('/HardwareVersion', 0)
         self._dbusservice.add_path('/Connected', 1)
-        #Create battery specific objects
+        # Create battery specific objects
         self._dbusservice.add_path('/Dc/Voltage', 24)
         self._dbusservice.add_path('/Dc/Current', 4)
         self._dbusservice.add_path('/Dc/Power', 100)
         self._dbusservice.add_path('/Dc/Temperature', 25)
         self._dbusservice.add_path('/Dc/Soc', 50)
-        #self._dbusservice.add_path('/Dc/TimeToGo', 10)A
+        # self._dbusservice.add_path('/Dc/TimeToGo', 10)A
 
         for path, settings in self._paths.iteritems():
             self._dbusservice.add_path(
                 path, settings['initial'], writeable=True, onchangecallback=self._handlechangedvalue)
 
-        self._ci = can.interface.Bus(channel='slcan0', bustype='socketcan')
-
-        gobject.timeout_add(1000, self._update)
+        self._ci = can.interface.Bus(channel='slcan0', bustype='socketcan', can_filters=[{"can_id": 0x0cf, "can_mask": 0xff0}])
+	self._bat = UbmsBattery() 
+	notifier = can.Notifier(self._ci, [self._bat])
+        gobject.timeout_add(10, self._update)
 
     def _update(self):
-        for message in self._ci:
-                print(message)
-
-        for path, settings in self._paths.iteritems():
-            if 'update' in settings:
-                self._dbusservice[path] = self._dbusservice[path] + settings['update']
-                logging.debug("%s: %s" % (path, self._dbusservice[path]))
+        self._dbusservice['/Dc/Soc'] = self._bat.soc 
+        self._dbusservice['/Dc/Current'] = self._bat.current 
         return True
 
     def _handlechangedvalue(self, path, value):
@@ -91,7 +101,8 @@ class DbusBatteryService:
 #
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    #logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     from dbus.mainloop.glib import DBusGMainLoop
     # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
