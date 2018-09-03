1e695e0a0da701c06fafc6ac45990d1df9fb42d0
gimx <einkauf.cn@gmail.com>
2018-09-03 21:02:09 +0000

    removed some unused dbus paths, added basic low SOC alarm, fixed pack voltage scaling, changed print to logging.debug

--- dbus_ubms.py
+++ dbus_ubms.py
@@ -31,6 +31,8 @@ class UbmsBattery(can.Listener):
 	self.underVoltageAlarm = 0
 	self.overVoltageAlarm = 0
 	self.overCurrentAlarm = 0
+	self.maxCellVoltage = 3.0
+	self.minCellVoltage = 3.0
 
     def on_message_received(self, msg):
 	if msg.arbitration_id == 0xc0:
@@ -38,28 +40,33 @@ class UbmsBattery(can.Listener):
 		self.underVoltageAlarm = msg.data[2] & 0x10
 		self.overVoltageAlarm = msg.data[2] & 0x20
 		self.overCurrentAlarm = msg.data[4] & 0x20
-		print("SOC: ",self.soc)
+		logging.debug("SOC: %d",self.soc)
 
-	if msg.arbitration_id == 0xc1:
-                self.voltage = msg.data[0]
+	elif msg.arbitration_id == 0xc1:
+                self.voltage = msg.data[0] * 2 #TODO make voltage scale factor input parameter
                 self.current = struct.unpack('b',chr(msg.data[1]))[0]
-		print("I: ",self.current, "U: ", self.voltage)
+		logging.debug("I: %dA U: %dV",self.current, self.voltage)
 
-	if msg.arbitration_id == 0xc4:
-		self.umax =  struct.unpack('<h', chr(msg.data[4])+chr(msg.data[5]))[0]*0.001
-		self.umin =  struct.unpack('<h', chr(msg.data[6])+chr(msg.data[7]))[0]*0.001
-		print("Umin ", self.umin, "Umax ", self.umax)
+	elif msg.arbitration_id == 0xc4:
+		self.maxCellVOltage =  struct.unpack('<h', chr(msg.data[4])+chr(msg.data[5]))[0]*0.001
+		self.minCellVoltage =  struct.unpack('<h', chr(msg.data[6])+chr(msg.data[7]))[0]*0.001
+		logging.debug("Umin %.3fV Umax %.3fV", self.minCellVoltage, self.maxCellVOltage)
 
-        if msg.arbitration_id in [0x46a, 0x46b]:
-                self.moduleCurrent = struct.unpack('>hhhh', ''.join(chr(i) for i in msg.data))
-                print("I ", self.moduleCurrent)
+        elif msg.arbitration_id in [0x46a, 0x46b]:
+                self.moduleCurrent = struct.unpack('>hhh', ''.join(chr(i) for i in msg.data[2:msg.dlc]))
+                logging.debug("mCurrents ", self.moduleCurrent)
+
+        elif msg.arbitration_id in [0x6a]:
+                self.moduleSoc = struct.unpack('BBBBBBB', ''.join(chr(i) for i in msg.data[1:msg.dlc]))
+                logging.debug("mSoc ", self.moduleSoc)
+
+
 
 
 
 class DbusBatteryService:
-    def __init__(self, servicename, deviceinstance, paths, productname='V*lence U-BMS', connection='CAN bus'):
+    def __init__(self, servicename, deviceinstance, productname='V*lence U-BMS', connection='CAN bus'):
         self._dbusservice = VeDbusService(servicename)
-        self._paths = paths
 
         logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
 
@@ -76,36 +83,41 @@ class DbusBatteryService:
         self._dbusservice.add_path('/HardwareVersion', 0)
         self._dbusservice.add_path('/Connected', 1)
         # Create battery specific objects
-        self._dbusservice.add_path('/Dc/0/Voltage', 24)
-        self._dbusservice.add_path('/Dc/0/Current', 4)
-        self._dbusservice.add_path('/Soc', 50)
+        self._dbusservice.add_path('/Dc/0/Voltage', 0)
+        self._dbusservice.add_path('/Dc/0/Power', 0)
+        self._dbusservice.add_path('/Dc/0/Current', 0)
+        self._dbusservice.add_path('/Soc', 10)
         self._dbusservice.add_path('/Dc/Temperature', 25)
-        self._dbusservice.add_path('/Info/MaxChargeCurrent', 50)
-        self._dbusservice.add_path('/Info/MaxDischargeCurrent', 50)
-        self._dbusservice.add_path('/Alarms/LowVoltage', 50)
-        self._dbusservice.add_path('/Alarms/HighVoltage', 50)
-        self._dbusservice.add_path('/Alarms/LowSoc', 50)
-        self._dbusservice.add_path('/Alarms/LowTemperature', 50)
-        self._dbusservice.add_path('/Alarms/HighTemperature', 50)
-        self._dbusservice.add_path('/Balancing', 50)
-        self._dbusservice.add_path('/System/NrOfBatteries', 50)
+#        self._dbusservice.add_path('/Info/MaxChargeCurrent', 150)
+#        self._dbusservice.add_path('/Info/MaxDischargeCurrent', 150)
+        self._dbusservice.add_path('/Alarms/LowVoltage', 0)
+        self._dbusservice.add_path('/Alarms/HighVoltage', 0)
+        self._dbusservice.add_path('/Alarms/LowSoc', 0)
+        self._dbusservice.add_path('/Alarms/LowTemperature', 0)
+        self._dbusservice.add_path('/Alarms/HighTemperature', 0)
+        self._dbusservice.add_path('/Balancing', 0)
+#        self._dbusservice.add_path('/System/NrOfBatteries', 8)
         self._dbusservice.add_path('/System/MinCellVoltage', 0.0)
         self._dbusservice.add_path('/System/MaxCellVoltage', 4.2)
 
 
-        for path, settings in self._paths.iteritems():
-            self._dbusservice.add_path(
-                path, settings['initial'], writeable=True, onchangecallback=self._handlechangedvalue)
-
-        self._ci = can.interface.Bus(channel='slcan0', bustype='socketcan', can_filters=[{"can_id": 0x0cf, "can_mask": 0xff0}])
+        self._ci = can.interface.Bus(channel='can0', bustype='socketcan', can_filters=[{"can_id": 0x0cf, "can_mask": 0xff0}])
 	self._bat = UbmsBattery() 
 	notifier = can.Notifier(self._ci, [self._bat])
         gobject.timeout_add(10, self._update)
 
     def _update(self):
+	if self._bat.soc < 10:
+		self._dbusservice['/Alarms/LowSoc'] = 1
+	else:
+		self._dbusservice['/Alarms/LowSoc'] = 0
+
         self._dbusservice['/Soc'] = self._bat.soc 
         self._dbusservice['/Dc/0/Current'] = self._bat.current 
         self._dbusservice['/Dc/0/Voltage'] = self._bat.voltage 
+        self._dbusservice['/Dc/0/Power'] = self._bat.voltage * self._bat.current 
+	self._dbusservice['/System/MinCellVoltage'] = self._bat.minCellVoltage
+        self._dbusservice['/System/MinCellVoltage'] = self._bat.maxCellVoltage
         return True
 
     def _handlechangedvalue(self, path, value):
@@ -132,13 +144,8 @@ def main():
 
     battery_output = DbusBatteryService(
         servicename='com.victronenergy.battery',
-        deviceinstance=0,
-        paths={
-            '/Dc/Battery/Soc': {'initial': 50, 'update': 1},
-            '/Dc/Battery/Voltage': {'initial': 24, 'update': 1},
-            '/Dc/Battery/Current': {'initial': 4, 'update': 1},
-            '/Dc/Battery/Power': {'initial': 100, 'update': 1},
-        })
+        deviceinstance=0
+        )
 
     logging.info('Connected to dbus, and switching over to gobject.MainLoop() (= event based)')
     mainloop = gobject.MainLoop()
