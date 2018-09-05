298fb410f901e61002c2b0017f16e56a3edeab41
gimx <einkauf.cn@gmail.com>
2018-09-05 13:18:57 +0000

    changed dbus warnings to be alarms

--- dbus_ubms.py
+++ dbus_ubms.py
@@ -4,7 +4,7 @@
 derived from dbusexample.py part of velib-python
 A class to put a battery service on the dbus, according to victron standards, with constantly updating
 paths. The data acquisition is done on CAN bus and decodes Valence U-BMS messages, in particular
-arbitration ID 0x0c4
+arbitration ID 0x0c0, 0xc1, 0xc4
 
 """
 import gobject
@@ -37,6 +37,7 @@ class UbmsBattery(can.Listener):
 	self.maxCellVoltage = 3.0
 	self.minCellVoltage = 3.0
 	self.partnr = 0 
+	self.firmwareVersion = 'unknown'
 	self.numberOfModules = 0
 
     def on_message_received(self, msg):
@@ -48,7 +49,7 @@ class UbmsBattery(can.Listener):
 		logging.debug("SOC: %d",self.soc)
 
 	elif msg.arbitration_id == 0xc1:
-                self.voltage = msg.data[0] * 4 #TODO make voltage scale factor input parameter
+                self.voltage = msg.data[0] * 1 #TODO make voltage scale factor input parameter
                 self.current = struct.unpack('b',chr(msg.data[1]))[0]
 		logging.debug("I: %dA U: %dV",self.current, self.voltage)
 
@@ -57,23 +58,24 @@ class UbmsBattery(can.Listener):
 		self.minCellVoltage =  struct.unpack('<h', chr(msg.data[6])+chr(msg.data[7]))[0]*0.001
 		logging.debug("Umin %.3fV Umax %.3fV", self.minCellVoltage, self.maxCellVOltage)
 
-        elif msg.arbitration_id in [0x46a, 0x46b]:
-                self.moduleCurrent = struct.unpack('>hhh', ''.join(chr(i) for i in msg.data[2:msg.dlc]))
-                logging.debug("mCurrents ", self.moduleCurrent)
+#        elif msg.arbitration_id in [0x46a, 0x46b]:
+#                self.moduleCurrent = struct.unpack('>hhh', ''.join(chr(i) for i in msg.data[2:msg.dlc]))
+#                logging.debug("mCurrents ", self.moduleCurrent)
 
-        elif msg.arbitration_id in [0x6a]:
-                self.moduleSoc = struct.unpack('BBBBBBB', ''.join(chr(i) for i in msg.data[1:msg.dlc]))
-                logging.debug("mSoc ", self.moduleSoc)
+#        elif msg.arbitration_id in [0x6a]:
+#                self.moduleSoc = struct.unpack('BBBBBBB', ''.join(chr(i) for i in msg.data[1:msg.dlc]))
+#                logging.debug("mSoc ", self.moduleSoc)
 
-	elif msg.arbitration_id == 0x184:
-		if msg.data[0] != 0xFF:
-			if msg.data[1] == 1:
-                		self.partnr = bytearray.fromhex(msg.data[2:7]).decode() 
-				logging.info(self.partnr)
-
-			if  msg.data[0] > self.numberOfModules: 
-				self.numberOfModules = msg.data[0]
-				logging.info(self.numberOfModules)
+#	elif msg.arbitration_id == 0x184:
+#		if msg.data[0] != 0xFF:
+#			#this is encoded battery information, not BMS 
+#			if msg.data[1] == 1: 
+#                		self.partnr = msg.data[2:7].decode() 
+#			elif msg.data[1] == 2:
+#				self.firmwareVersion = msg.data[2:7].decode()
+#
+#			if  msg.data[0] > self.numberOfModules: 
+#				self.numberOfModules = msg.data[0]
 
 
 
@@ -101,8 +103,8 @@ class DbusBatteryService:
         self._dbusservice.add_path('/Dc/0/Current', 0)
         self._dbusservice.add_path('/Soc', 10)
         self._dbusservice.add_path('/Dc/Temperature', 25)
-#        self._dbusservice.add_path('/Info/MaxChargeCurrent', 150)
-#        self._dbusservice.add_path('/Info/MaxDischargeCurrent', 150)
+        self._dbusservice.add_path('/Info/MaxChargeCurrent', 150)
+        self._dbusservice.add_path('/Info/MaxDischargeCurrent', 150)
         self._dbusservice.add_path('/Alarms/LowVoltage', 0)
         self._dbusservice.add_path('/Alarms/HighVoltage', 0)
         self._dbusservice.add_path('/Alarms/LowSoc', 0)
@@ -110,28 +112,33 @@ class DbusBatteryService:
         self._dbusservice.add_path('/Alarms/HighTemperature', 0)
         self._dbusservice.add_path('/Balancing', 0)
         self._dbusservice.add_path('/System/NrOfBatteries', 8)
-        self._dbusservice.add_path('/System/MinCellVoltage', 0.0)
+        self._dbusservice.add_path('/System/MinCellVoltage', 3.0)
         self._dbusservice.add_path('/System/MaxCellVoltage', 4.2)
 
 
-        self._ci = can.interface.Bus(channel='can0', bustype='socketcan', can_filters=[{"can_id": 0x0cf, "can_mask": 0xff0}])
+        self._ci = can.interface.Bus(channel=connection, bustype='socketcan', can_filters=[{"can_id": 0x0cf, "can_mask": 0xff0}])
 	self._bat = UbmsBattery() 
 	notifier = can.Notifier(self._ci, [self._bat])
-        gobject.timeout_add(10, self._update)
+        gobject.timeout_add(1000, self._update)
 
     def _update(self):
-	if self._bat.soc < 10:
-		self._dbusservice['/Alarms/LowSoc'] = 1
+#	self._dbusservice['/FirmwareVersion'] = self._bat.firmwareVersion
+#	self._dbusservice['/HardwareVersion'] = self._bat.partnr
+	
+	if self._bat.soc < 5:
+		self._dbusservice['/Alarms/LowSoc'] = 2 
+	elif self._bat.soc < 10:
+		self._dbusservice['/Alarms/LowSoc'] = 1 
 	else:
 		self._dbusservice['/Alarms/LowSoc'] = 0
 
 	if self._bat.underVoltageAlarm != 0:
-	 	self._dbusservice['/Alarms/LowVoltage'] = 1 
+	 	self._dbusservice['/Alarms/LowVoltage'] = 2 
 	else:
 	 	self._dbusservice['/Alarms/LowVoltage'] = 0 
 
  	if self._bat.overVoltageAlarm:
-                self._dbusservice['/Alarms/HighVoltage'] = 1
+                self._dbusservice['/Alarms/HighVoltage'] = 2 
         else:
                 self._dbusservice['/Alarms/HighVoltage'] = 0
 
@@ -141,6 +148,7 @@ class DbusBatteryService:
         self._dbusservice['/Dc/0/Power'] = self._bat.voltage * self._bat.current 
 	self._dbusservice['/System/MinCellVoltage'] = self._bat.minCellVoltage
         self._dbusservice['/System/MinCellVoltage'] = self._bat.maxCellVoltage
+	self._dbusservice['/System/NrOfBatteries'] =  self._bat.numberOfModules
         return True
 
     def _handlechangedvalue(self, path, value):
@@ -150,12 +158,10 @@ class DbusBatteryService:
 
 # === All code below is to simply run it from the commandline for debugging purposes ===
 
-# It will created a dbus service called com.victronenergy.battery.ubms.0
+# It will created a dbus service called com.victronenergy.battery
 # To try this on commandline, start this program in one terminal, and try these commands
 # from another terminal:
-# dbus com.victronenergy.battery.ubms
-# dbus com.victronenergy.battery.ubms /Dc/Soc GetValue
-#
+# dbus -y com.victronenergy.battery /Dc/Soc GetValue
 
 def main():
     parser = ArgumentParser(description='dbus-modem', add_help=True)
