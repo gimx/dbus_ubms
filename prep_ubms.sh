58e82761a80b62b3faf84622367a4ca496e10923
gimx <einkauf.cn@gmail.com>
2019-02-23 09:04:47 +0000

    added preparation script for installation of python modules

---
+++ prep_ubms.sh
@@ -0,0 +1,6 @@
+/opt/victronenergy/swupdate-scripts/set-feed.sh candidate
+opkg update
+opkg install python-misc python-distutils python-numbers python-html python-ctypes python-pkgutil
+opkg install python-unittest python-difflib python-compile gcc binutils python-dev python-unixadmin python-xmlrpc
+python get-pip.py
+pip install python-can
