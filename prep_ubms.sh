f1057fc8bd2a2ebb91e341c41a7f5e161af9f430
gimx <einkauf.cn@gmail.com>
2019-05-31 17:08:44 +0000

    removed some unecessary packages

--- prep_ubms.sh
+++ prep_ubms.sh
@@ -1,6 +1,7 @@
 /opt/victronenergy/swupdate-scripts/set-feed.sh candidate
 opkg update
-opkg install python-misc python-distutils python-numbers python-html python-ctypes python-pkgutil
-opkg install python-unittest python-difflib python-compile gcc binutils python-dev python-unixadmin python-xmlrpc
+opkg install python-misc python-distutils python-ctypes python-pkgutil
+opkg install python-xmlrpc python-unixadmin
+#opkg install python-unittest python-difflib python-compile gcc binutils python-dev 
 python get-pip.py
 pip install python-can
