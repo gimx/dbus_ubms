65896c843ec65c4d0e909a4a796650bfb1dfb497
gimx <einkauf.cn@gmail.com>
2022-05-19 10:07:14 +0000

    change to python3 modules

--- prep_ubms.sh
+++ prep_ubms.sh
@@ -1,7 +1,8 @@
-/opt/victronenergy/swupdate-scripts/set-feed.sh candidate
+#/opt/victronenergy/swupdate-scripts/set-feed.sh candidate
+/opt/victronenergy/swupdate-scripts/set-feed.sh release
 opkg update
-opkg install python-misc python-distutils python-ctypes python-pkgutil
-opkg install python-xmlrpc python-unixadmin
-#opkg install python-unittest python-difflib python-compile gcc binutils python-dev 
+opkg install python3-misc python3-distutils python3-ctypes python3-pkgutil
+opkg install python3-xmlrpc python3-unixadmin
+#opkg install python3-unittest python3-difflib python3-compile gcc binutils python3-dev 
 python get-pip.py
 pip install python-can
