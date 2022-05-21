99afcb963969507a35d52edfd6ea98886986b08e
gimx <einkauf.cn@gmail.com>
2022-05-21 11:25:41 +0000

    change to install pip from repo

--- prep_ubms.sh
+++ prep_ubms.sh
@@ -4,5 +4,6 @@ opkg update
 opkg install python3-misc python3-distutils python3-ctypes python3-pkgutil
 opkg install python3-xmlrpc python3-unixadmin
 #opkg install python3-unittest python3-difflib python3-compile gcc binutils python3-dev 
-python get-pip.py
-pip install python-can
+opkg install python-pip
+#python get-pip.py
+pip3 install python-can
