9ecb561bf5ae9c8d866e5c4ad0a641cb9997fbfa
Torsten <torsten.vogel@gmx.net>
2023-03-08 18:54:02 +0000

    changed to python3 pip install

--- prep_ubms.sh
+++ prep_ubms.sh
@@ -4,6 +4,6 @@ opkg update
 opkg install python3-misc python3-distutils python3-ctypes python3-pkgutil
 opkg install python3-xmlrpc python3-unixadmin
 #opkg install python3-unittest python3-difflib python3-compile gcc binutils python3-dev 
-opkg install python-pip
-#python get-pip.py
+opkg install python3-pip
+#curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 pip3 install python-can
