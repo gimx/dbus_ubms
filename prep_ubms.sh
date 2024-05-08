46d1203cb27057d537d3eaecd2f59c211361ce32
gimx <torsten.vogel@gmx.net>
2024-05-08 09:58:00 +0000

    updated depencies for venusOS v3.xx

--- prep_ubms.sh
+++ prep_ubms.sh
@@ -1,9 +1,2 @@
-#/opt/victronenergy/swupdate-scripts/set-feed.sh candidate
-/opt/victronenergy/swupdate-scripts/set-feed.sh release
 opkg update
-opkg install python3-misc python3-distutils python3-ctypes python3-pkgutil
-opkg install python3-xmlrpc python3-unixadmin
-#opkg install python3-unittest python3-difflib python3-compile gcc binutils python3-dev 
-opkg install python3-pip
-#curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
-pip3 install python-can
+opkg install python3-can
